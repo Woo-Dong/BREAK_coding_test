30f4cf5378566670079b8ff7339aeb8effa74cb0
Woo-Dong <dwpark2007@naver.com>
2020-07-02 18:37:53 +0900

    0702

--- Level1/Graph_BFS/1_13023.py
+++ Level1/Graph_BFS/1_13023.py
@@ -1,32 +1,29 @@
 
-# Not solved
 import sys 
 
 n, m = map(int, input().split()) 
 
-adict = dict() 
-
-check = [False]*n 
-def dfs(node, cnt): 
-    for elem in adict[node]: 
-        if not check[elem]:
-            check[elem] = True
-            if cnt == 3: 
-                print(1) 
-                exit() 
-            dfs(elem, cnt+1) 
+edges = list() 
+node_dict = dict() 
 
 for _ in range(m): 
-    x, y = map(int, input().split()) 
-    if x not in adict: adict[x] = [y] 
-    else: adict[x].append(y) 
+    a, b = map(int, input().split()) 
+    edges.append((a, b)) 
+    edges.append((b, a)) 
+    if a not in node_dict: node_dict[a] = [b] 
+    else: node_dict[a].append(b) 
 
-    if y not in adict: adict[y] = [x] 
-    else: adict[y].append(x) 
+    if b not in node_dict: node_dict[b] = [a] 
+    else: node_dict[b].append(a) 
 
-for elem in adict:
-    check = [False]*n 
-    check[elem] = True 
-    dfs(elem, 0) 
-
-print(0)
+for i in range(2*m): 
+    a, b = edges[i] 
+    for j in range(2*m): 
+        c, d = edges[j] 
+        tmp_set = set([a, b, c, d]) 
+        if len(tmp_set) < 4 or c not in node_dict[b]: continue 
+        for e in node_dict[d]: 
+            if e in tmp_set: continue 
+            print(1) 
+            sys.exit() 
+print(0) 
--- Level1/Graph_BFS/4_1707.py
+++ Level1/Graph_BFS/4_1707.py
@@ -1,15 +1,36 @@
 
-# Not solved
+import sys 
+sys.setrecursionlimit(100000000)
+
 k = int(input()) 
 for _ in range(k): 
-    v, e = map(int, input().split()) 
+    v, e = map(int, sys.stdin.readline().split())  
     
-    arr = [[0]*v for _ in range(v)] 
+    adict = dict() 
 
     for _ in range(e): 
-        a, b = map(int, input().split()) 
-        a -= 1
-        b -= 1
-        arr[a][b] = arr[b][a] = 1 
-    
+        a, b = map(int, sys.stdin.readline().split()) 
+        if a not in adict: adict[a] = [b] 
+        else: adict[a].append(b) 
 
+        if b not in adict: adict[b] = [a] 
+        else: adict[b].append(a) 
+
+    color = [0] * (v+1) 
+
+    def dfs(x, c): 
+        color[x] = c 
+        if x not in adict: return True 
+        for elem in adict[x]: 
+            if color[elem] == 0 and not dfs(elem, 3-c): return False 
+            elif color[elem] == color[x]: return False 
+        return True 
+    
+    ans = True 
+    for i in range(1, v+1): 
+        if color[i] == 0 and not dfs(i, 1): 
+            ans = False 
+            break 
+    
+    if ans: print("YES")
+    else: print("NO")
